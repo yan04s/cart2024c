469db487c7c98cf21924ee30d9234b612447c1db
yan04s <tan828825@gmail.com>
2024-11-19 16:43:06 +0800

    s

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,8 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Product;
 
 class Category extends Model
 {
-    //
+    use HasFactory;
+    protected $fillable = ['name'];
+
+    public function category()
+    {
+        return $this->hasMany(Product::class);
+    }
 }
---
+++ database/migrations/2024_11_18_033651_create_products_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            //$table->decimal('price', 10, 2);
+            $table->double('price', 8, 2);//digit, decimal
+            $table->string('image');
+            $table->integer('quantity')->unsigned();// positive integer //signed() is default and can be negative integer // https://www.ibm.com/docs/en/aix/7.2?topic=types-signed-unsigned-integers
+            $table->foreignId('categoryID');//->constrained()->onDelete('cascade');//categoryID is the column name in the products table
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};

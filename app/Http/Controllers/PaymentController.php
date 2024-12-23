3fb7ac75a3b5bee86b8bbcb436e7e34ae01901a5
yan04s <tan828825@gmail.com>
2024-12-24 03:48:51 +0800

    h

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -4,19 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use Stripe;
+use Stripe\Stripe;
+use Stripe\Charge;
 
 class PaymentController extends Controller
 {
     public function paymentPost(Request $request)
     {
 	       
-	Stripe\Stripe::setApiKey(env('STRIPE_SECRET'));
-        Stripe\Charge::create ([
-                "amount" => $request->sub*100,   // RM10  10=10 cen 10*100=1000 cen
-                "currency" => "MYR",
-                "source" => $request->stripeToken,
-                "description" => "This payment is testing purpose of southern online",
+	    Stripe::setApiKey(env('STRIPE_SECRET'));
+        Charge::create ([
+            "amount" => $request->sub*100,   // RM10  10=10 cen 10*100=1000 cen
+            "currency" => "MYR",
+            "source" => $request->stripeToken,
+            "description" => "This payment is testing purpose of southern online",
         ]);
            
         return back();

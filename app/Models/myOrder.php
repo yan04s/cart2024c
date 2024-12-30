3023c5f200e2eef06ab6b9bb2f2b691036df2ad2
yan04s <tan828825@gmail.com>
2024-12-30 11:05:18 +0800

    d

--- app/Models/myOrder.php
+++ app/Models/myOrder.php
@@ -2,9 +2,11 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class myOrder extends Model
 {
+    use HasFactory;
     protected $fillable=['paymentStatus','userID','amount'];
 }
